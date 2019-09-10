5078567663169d9962e43c27a1b43ed7a756f849
弘毅 <goodmenhy@163.com>
2019-09-10 11:09:18 +0800

    添加rem

---
+++ remxiao.js
@@ -0,0 +1,14 @@
+
+(function (doc, win) {
+    var docEl = doc.documentElement
+    var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
+    var recalc = function () {
+      var clientWidth = docEl.clientWidth
+      if (!clientWidth) return
+      docEl.style.fontSize = 200 * (clientWidth / 750) + 'px'
+    }
+    if (!doc.addEventListener) return
+    win.addEventListener(resizeEvt, recalc, false)
+    doc.addEventListener('DOMContentLoaded', recalc, false)
+  })(document, window)
+  
